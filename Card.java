5a27fbea760934b1d912b19bdeef8d4229bf2a8d
Matthew Fischer <mattfisc@gmail.com>
2018-12-07 22:38:31 -0800

    working deck of cards. BOOM!!!

--- Card.java
+++ Card.java
@@ -4,54 +4,69 @@ package oopsprac;
 
 public class Card {
     // CONSTATANT VARIABLES DECLARED
-    public static final int SPADE = 1;
-    public static final int DIAMOND = 2;
-    public static final int CLUB = 3;
-    public static final int HEART = 4;
+    public static final int SPADE = 0;
+    public static final int DIAMOND = 1;
+    public static final int CLUB = 2;
+    public static final int HEART = 3;
 
     // VARIABLES DECLARED
     private int suit;
     private int rank;
-    public final Card[][] deck = new Card[4][14];
+    
+    
+    
     
     public Card(int s, int r){
         suit = s;
         rank = r;
     }
     
-    public Card(){
+    public String getSuit(){
+        String strSuit = "";
         
-    }
-    
-    public int getRank(){
-        return rank;
-    }
-    public void setRank(int r){
-        rank = r;
-    }
-    public int getSuit(){
-        return suit;
-    }
-    public void setSuit(int s){
-        suit = s;
-    }
-    public String toString(){
-        String strSuit = Integer.toString(suit);
+        switch(suit){
+            case 0:
+                strSuit = "Spade";
+                break;
+            case 1:
+                strSuit = "Diamond";
+                break;
+            case 2: 
+                strSuit = "Club";
+                break;
+            case 3:
+                strSuit = "Heart";
+                break;
+            default:
+                strSuit = "Error suit";
+        }
+        
         return strSuit;
     }
-//    public void initializeDeck(){
-//        for(int i = 1; i <= deck.length; i++){
-//            for(int j = 1; j <= deck[i].length; j++){
-//                deck[i][j] = new Card(i,j);
-//            }
-//        }
-//    }
-    public void setDeckOfCards(){
-        for(int i = 1; i <= 4; i++){
-            for(int j = 1; j <= 14; j++){
-                deck[i][j] = new Card(i,j);
-            }
-        }
-    }
     
+    public String getRank(){
+        String strRank = "";
+        switch(rank){
+            case 0:
+                strRank = "Ace";
+                break;
+                
+            case 10:
+                strRank = "Jack";
+                break;
+                
+            case 11:
+                strRank = "Queen";
+                break;
+                
+            case 12:
+                strRank = "King";
+                break;
+            default:
+                strRank += rank;
+        }
+        
+        return strRank;
+    }
+  
 }
--- Oopsprac.java
+++ Oopsprac.java
@@ -3,14 +3,71 @@ package oopsprac;
 
 public class Oopsprac {
 
-
     public static void main(String[] args) {
+
+        // intialize deck array
+        Card[] deck = new Card[52];
+        Card[] hand = new Card[5];
         
-        Card deck = new Card();
+        // assign deck a card
+        assignDeck(deck);
         
-        // says it is not initialized
-        deck.setDeckOfCards();
+        // shuffle deck
+        shuffle(deck);
         
+        //assign hand of 5 cards
+        get5Cards(hand,deck);
+        
+        
+        // output hand
+        output(hand);
     }
     
+    // ---------------Method assignDeck----------------------
+    public static void assignDeck(Card[] deck){
+        for(int i = 0; i < 52; i++){
+
+            int suit = i / 13;
+            int rank = i % 13;
+
+            deck[i] = new Card(suit,rank);
+        }
+    }
+    
+    //-------------------- Output ----------------
+    public static void output(Card[] c){
+        for(int i = 0; i < c.length-1; i++){
+            System.out.println( c[i].getRank() + " of " + c[i].getSuit() );
+        }
+    }
+    
+    //------------- Shuffle -------------------------
+    public static void shuffle(Card[] deck){
+        for(int i = 0; i < deck.length; i++){
+            
+            int mix1 = (int)(Math.random() * deck.length);
+            int mix2 = (int)(Math.random() * deck.length);
+            
+            Card temp = deck[mix1];
+            deck[mix1] = deck[mix2];
+            deck[mix2] = temp;
+        }
+    }
+    
+    //---------------- get5cards -----------------------
+    public static void get5Cards(Card[] hand, Card[] deck){
+        
+        for(int i = 0; i < hand.length-1; ){
+            
+            int rnd = (int)(Math.random() * 52);
+            
+            // take card out of deck
+            if(deck[rnd] != null){
+                hand[i] = deck[rnd];
+                deck[rnd] = null;
+                i++;
+            }
+        }
+    }
 }
+
